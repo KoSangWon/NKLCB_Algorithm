a48ad691cc8bcd96ed123d120f6d6de8eb8c71dc
Sangwon <jackoss@naver.com>
2021-04-09 20:31:14 +0900

    feat: Add QuickSort

--- day7/QuickSort.py
+++ day7/QuickSort.py
@@ -1,29 +1,45 @@
-def quick_sort(x, low, high):
-    if high <= low:
-        return
+# In-place O(logN)
+def quick_sort(x):
+    def partition(low, high):
+        pivot = x[(low + high) // 2]
+        while low <= high: # mid에서 만나면 탈출(즉, low == mid인 경우 탈출)
+            while x[low] < pivot:
+                low += 1
+            while x[high] > pivot:
+                high -= 1
 
-    pivot = x[low]
-    i, j = low, high
+            if low <= high:
+                x[low], x[high] = x[high], x[low]
+                low, high = low + 1, high - 1
 
-    while i < j:
-        while j > low:
-            if x[j] < pivot:
-                break
-            j -= 1
-            
-        while i < high:
-            if x[i] > pivot:
-                break
-            i += 1
+        return low
+
+    def sort(low, high):
+        if high <= low: # 크기가 0, 1인 경우 처리
+            return
         
-        if i < j:
-            x[i], x[j] = x[j], x[i]
-    
-    x[low], x[j] = x[j], x[low]
-    
-    quick_sort(x, 0, j - 1)
-    quick_sort(x, j + 1, high)
+        mid = partition(low, high)
+        sort(low, mid - 1)
+        sort(mid, high)
 
-x = [5,4,3,1,2,6,3]
-quick_sort(x, 0, len(x)-1)
-print(x) # [1, 2, 3, 3, 4, 5, 6]
+    return sort(0, len(x) - 1)
+
+
+# 공간복잡도 O(NlogN)
+def quicksort(x):
+    if len(x) <= 1:
+        return x
+
+    pivot = x[len(x) // 2]
+    left = []
+    right = []
+    mid = []
+    for a in x:
+        if a < pivot:
+            left.append(a)
+        elif a > pivot:
+            right.append(a)
+        else:
+            mid.append(a)
+
+    return quicksort(left) + mid + quicksort(right)
